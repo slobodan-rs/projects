b45281d6d86463fb85fd83515681dd8a966b7ec4
Slobodan <mr.slobodan@hotmail.rs>
2020-10-21 13:30:13 +0200

    quiz app

---
+++ Quiz-React-Typescript/quiz/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { GlobalStyle } from './components/GlobalStyle';
+
+function App() {
+  return (
+    <>
+    <GlobalStyle/>
+      <p>asdsasdasdd</p>
+    </>
+  );
+}
+
+export default App;
